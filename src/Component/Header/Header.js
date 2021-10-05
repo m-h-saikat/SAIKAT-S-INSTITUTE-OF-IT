a9f25399d4385616355288da410b321ddf4f03a7
Mehedi Hasan Saikat <29018897+mehedi-hasan-saikat@users.noreply.github.com>
2021-10-05 15:47:18 +0600

    nav ui update

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,40 +4,63 @@ import Logo from "../../Image/4792249.jpg";
 import "./Header.css";
 const Header = () => {
   return (
-    <div className="  header d-flex align-items-center justify-content-between px-5">
-      <div className="ms-5">
-        <img
-          className="my-auto logo p-2"
-          src={Logo}
-          alt=""
-          width="60"
-          height="60"
-        />
-      </div>
-      <div className="menu-container d-flex me-5">
-        <ul className="navlink align-items-end justify-content-end">
-          <Link to="/Home" className="items">
-            <li>
-              <button className="btn btn-white ">Home</button>
-            </li>
-          </Link>
-          <Link to="/AboutUs" className="items">
-            <li>
-              <button className="btn btn-white ">About Us</button>
-            </li>
-          </Link>
-          <Link to="/Service" className="items">
-            <li>
-              <button className="btn btn-white ">Course</button>
-            </li>
-          </Link>
-          <Link to="contact" className="items">
-            <li>
-              <button className="btn btn-white ">Contact</button>
-            </li>
-          </Link>
-        </ul>
-      </div>
+    <div>
+      <nav className="m-auto navbar navbar-expand-md navbar-dark ">
+        <div className="container ">
+          <img
+            className="my-auto logo  ms-5"
+            src={Logo}
+            alt=""
+            width="60"
+            height="60"
+          />
+        </div>
+        <div className="me-1">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className=" navbar-nav me-auto mb-lg-0">
+              <li className="nav-item">
+                <Link to="/Home" className="items">
+                  <li>
+                    <button className="btn btn-white me-4 ">Home</button>
+                  </li>
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link to="/AboutUs" className="items">
+                  <li>
+                    <button className="btn btn-white me-4 ">About</button>
+                  </li>
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link to="/Service" className="items">
+                  <li>
+                    <button className="btn btn-white me-4">Course</button>
+                  </li>
+                </Link>{" "}
+              </li>
+              <li className="nav-item">
+                <Link to="contact" className="items">
+                  <li>
+                    <button className="btn btn-white me-4 ">Contact</button>
+                  </li>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
